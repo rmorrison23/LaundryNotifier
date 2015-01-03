e0ea1a73c26339e25e83ff94c627e85f7434f139
Ryan Morrison <morrisor@uci.edu>
2015-01-02 21:31:49 -0800

    ignore

--- laundryInterface/php/washerDone.php
+++ laundryInterface/php/washerDone.php
@@ -3,7 +3,11 @@
 	$fp = fopen('../data/xivelyFeed.json', 'w');
     fwrite($fp, "{\"version\":\"1.0.0\",\"datastreams\":[{\"id\":\"Washer\",\"current_value\":\"0\"}]}");
     fclose($fp);
-	
+
+	$fp = fopen('../data/machineStatus.txt', 'w');
+	fwrite($fp, "*Washer Off\n");
+	fclose($fp);
+		
 	$output = shell_exec("./scripts/sendXivelyFeed.sh");
     //echo "<pre>$output</pre>";
 
